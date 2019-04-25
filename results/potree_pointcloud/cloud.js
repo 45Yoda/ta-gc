7ceefa187bb85e36aa08b1c52d1bfc7741e72f68
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-04-25 12:18:13 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 21683777,
+    "boundingBox": {
+        "lx": 554034.4134308703,
+        "ly": 4505755.038835806,
+        "lz": -18.077693263315696,
+        "ux": 554445.0869885048,
+        "uy": 4506165.71239344,
+        "uz": 392.5958643711497
+    },
+    "tightBoundingBox": {
+        "lx": 554034.41,
+        "ly": 4505755.04,
+        "lz": -18.080000000000003,
+        "ux": 554445.09,
+        "uy": 4506090.37,
+        "uz": 62.61
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.8452298641204836,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
